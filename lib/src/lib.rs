efd1da093eafe1e25378c409f67e69fcf71635d5
Wilfried Kopp <wilfried@parity.io>
2021-11-09 18:06:02 +0100

    Add new test

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -12,8 +12,14 @@ mod tests {
     use crate::Calculator;
 
     #[test]
-    fn it_works() {
+    fn it_adds_1_and_3() {
         let result = Calculator::add(1, 3);
         assert_eq!(result, 4);
+    }
+
+
+    #[test]
+    fn it_adds_2_and_2() {
+        assert_eq!(4, Calculator::add(2, 2));
     }
 }
